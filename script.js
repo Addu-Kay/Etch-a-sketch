e75361fbbc8d85882d0c70638a69617b0a524799
Addu Kay <adarshackerm4n@gmail.com>
2024-06-19 16:49:30 +0530

    improve code

--- script.js
+++ script.js
@@ -1,15 +1,97 @@
+// DOM Elements
 const cellDisplay = document.getElementById("cellDisplay");
 const slider = document.getElementById("range");
-const DEFAULT_NUMBER_OF_COLUMN_AND_ROW_CELLS = 4;
-slider.value = DEFAULT_NUMBER_OF_COLUMN_AND_ROW_CELLS;
-cellDisplay.textContent = `${slider.value}x${slider.value}`;
+const colorPicker = document.getElementById("color");
 const canvas = document.querySelector(".canvas");
-updateNumberOfCells(DEFAULT_NUMBER_OF_COLUMN_AND_ROW_CELLS);
+const clearButton = document.getElementById("clear");
+const eraseButton = document.getElementById("eraser");
+const colorButton = document.getElementById("color-mode");
+const rainbowButton = document.getElementById("rainbow-mode");
+const buttonArray = Array.from(document.getElementsByTagName("button"));
 
+// CONSTANTS
+const DEFAULT_NUMBER_OF_COLUMN_AND_ROW_CELLS = 4;
+
+// variables
+let color = colorPicker.value;
+let erase = false;
+let rainbowMode = false;
+let mouseDown = false;
+
+// Event listeners
+canvas.addEventListener("mouseover", handleCanvasEvent);
+canvas.addEventListener("mousedown", handleCanvasEvent);
+colorPicker.addEventListener("input", (event) => {
+  color = event.target.value;
+});
+colorButton.addEventListener("click", () => setMode("color"));
+rainbowButton.addEventListener("click", () => setMode("rainbowMode"));
+eraseButton.addEventListener("click", () => setMode("erase"));
+clearButton.addEventListener("click", clearCanvas);
+document.addEventListener("mousedown", (event) => {
+  if (event.target.classList.contains("canvas-item")) {
+    mouseDown = true;
+  }
+});
+document.addEventListener("mouseup", () => (mouseDown = false));
 slider.addEventListener("input", () => {
   clearGrid();
   updateNumberOfCells(slider.value);
 });
+buttonArray.forEach((button, index) => {
+  if (!(index == buttonArray.length - 1)) {
+    button.addEventListener("click", (event) => {
+      document
+        .getElementsByClassName("selected")[0]
+        .classList.remove("selected");
+      event.target.classList.add("selected");
+    });
+  }
+});
+
+// initialization
+function initialize() {
+  slider.value = DEFAULT_NUMBER_OF_COLUMN_AND_ROW_CELLS;
+  updateNumberOfCells(DEFAULT_NUMBER_OF_COLUMN_AND_ROW_CELLS);
+}
+
+// Event handlers
+function handleCanvasEvent(event) {
+  if (event.type === "mouseover" && !mouseDown) return;
+  if (event.target.classList.contains("canvas-item")) {
+    draw(event);
+  }
+}
+
+// functions
+function clearCanvas() {
+  const canvasItemArray = Array.from(
+    document.getElementsByClassName("canvas-item")
+  );
+  canvasItemArray.forEach((canvasItem) => {
+    canvasItem.style.background = "white";
+  });
+}
+
+function draw(event) {
+  if (erase) {
+    color = "white";
+    event.target.style.background = color;
+  } else if (rainbowMode) {
+    color = randomColor();
+    event.target.style.background = color;
+  } else {
+    event.target.style.background = color;
+  }
+}
+
+function setMode(mode) {
+  erase = mode === "erase";
+  rainbowMode = mode === "rainbowMode";
+  if (mode === "color") {
+    color = colorPicker.value;
+  }
+}
 
 function clearGrid() {
   canvas.innerHTML = "";
@@ -26,29 +108,6 @@ function updateNumberOfCells(sliderValue) {
   }
 }
 
-canvas.addEventListener("mouseover", draw);
-canvas.addEventListener("mousedown", draw);
-
-let colorPicker = document.getElementById("color");
-let color = colorPicker.value;
-let erase = false;
-let rainbowMode = false;
-
-function draw(event) {
-  console.log(mouseDown);
-  if (event.type === "mouseover" && !mouseDown) return;
-  if (event.target.classList.contains("canvas-item")) {
-    if (erase) {
-      event.target.style.background = "white";
-    } else if (rainbowMode) {
-      color = randomColor();
-      event.target.style.background = color;
-    } else {
-      event.target.style.background = color;
-    }
-  }
-}
-
 function randomColor() {
   const randomR = Math.floor(Math.random() * 256);
   const randomG = Math.floor(Math.random() * 256);
@@ -56,59 +115,4 @@ function randomColor() {
   return `rgb(${randomR},${randomG},${randomB})`;
 }
 
-colorPicker.addEventListener("input", (event) => {
-  color = event.target.value;
-});
-
-const clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", () => {
-  const canvasItemArray = Array.from(
-    document.getElementsByClassName("canvas-item")
-  );
-  canvasItemArray.forEach((canvasItem) => {
-    canvasItem.style.background = "white";
-  });
-});
-
-const eraseButton = document.getElementById("eraser");
-eraseButton.addEventListener("click", () => {
-  erase = true;
-  rainbowMode = false;
-});
-
-const colorButton = document.getElementById("color-mode");
-colorButton.addEventListener("click", () => {
-  color = colorPicker.value;
-  erase = false;
-  rainbowMode = false;
-});
-
-const rainbowButton = document.getElementById("rainbow-mode");
-rainbowButton.addEventListener("click", () => {
-  erase = false;
-  rainbowMode = true;
-});
-
-const buttonArray = Array.from(document.getElementsByTagName("button"));
-buttonArray.forEach((button, index) => {
-  if (!(index == buttonArray.length - 1)) {
-    button.addEventListener("click", (event) => {
-      document
-        .getElementsByClassName("selected")[0]
-        .classList.remove("selected");
-      event.target.classList.add("selected");
-    });
-  }
-});
-
-let mouseDown = false;
-
-document.onmousedown = (event) => {
-  if (event.target.classList.contains("canvas-item")) {
-    mouseDown = true;
-  }
-};
-
-document.onmouseup = () => {
-  mouseDown = false;
-};
+initialize();
